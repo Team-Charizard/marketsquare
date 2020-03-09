339c6a37f937476347949f829a4a201915378872
Kelvin Cuesta <kelvinscuesta@gmail.com>
2020-03-09 14:58:17 -0400

    Setup boilerplate

---
+++ client/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/extensions */
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App.jsx';
+// reminder to get scss or css in here
+// import './css/styles.css';
+
+render(<App />, document.getElementById('root'));
---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use('/build', (req, res) =>
+  res.sendFile(path.resolve(__dirname, '../build/bundle.js')),
+);
+
+app.listen(PORT, () => console.log(`SERVER LISTENING ON ${PORT}`));
